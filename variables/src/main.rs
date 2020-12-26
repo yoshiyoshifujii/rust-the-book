4f400047293ca7425e93a6310f24fdd262b6f690
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2020-12-26 12:01:43 +0900

    3章まで

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,6 +3,14 @@ fn main() {
     mut_and_immutable();
     var_and_constants();
     shadowing();
+
+    let x = five();
+    println!("The value of x is: {}", x);
+
+    let x = plus_one(5);
+    println!("The value of x is: {}", x);
+
+    for_loop();
 }
 
 fn mut_and_immutable() {
@@ -26,4 +34,20 @@ fn shadowing() {
     let x = x * 2;
 
     println!("The value of x is: {}", x);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn for_loop() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("the value is: {}", element)
+    }
 }
