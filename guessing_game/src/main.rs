0bcb75f43b1eea7c46c30b26e647a3c79293ed80
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2020-12-26 09:08:59 +0900

    予想と秘密の数字を比較する
    https://doc.rust-jp.rs/book-ja/ch02-00-guessing-game-tutorial.html#%E4%BA%88%E6%83%B3%E3%81%A8%E7%A7%98%E5%AF%86%E3%81%AE%E6%95%B0%E5%AD%97%E3%82%92%E6%AF%94%E8%BC%83%E3%81%99%E3%82%8B

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,8 @@
 extern crate rand;
 
+use core::cmp::Ordering;
 use std::io;
+
 use rand::Rng;
 
 fn main() {
@@ -18,5 +20,15 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim()
+        .parse()
+        .expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
