cc9a448a969e108f8c29246dbb7aefc8b3388f24
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2021-01-23 21:40:33 +0900

    module

---
+++ restaurant/src/front_of_house.rs
@@ -0,0 +1,4 @@
+pub mod hosting {
+    pub fn add_to_wait_list() {}
+    fn seat_at_table() {}
+}
--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,3 +1,5 @@
+mod front_of_house;
+
 #[cfg(test)]
 mod tests {
     use crate::eat_at_restaurant2;
@@ -6,19 +8,6 @@ mod tests {
     fn it_works() {
         eat_at_restaurant2();
         assert_eq!(2 + 2, 4);
-    }
-}
-
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_wait_list() {}
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-        fn serve_order() {}
-        fn take_payment() {}
     }
 }
 
