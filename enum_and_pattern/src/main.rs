7cefddedf3197b55042f5f9ed27f8d8cda78a00a
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2021-01-23 16:12:49 +0900

    Option<T> and match

--- enum_and_pattern/src/main.rs
+++ enum_and_pattern/src/main.rs
@@ -23,8 +23,14 @@ fn main() {
             address: String,
         }
 
-        let home = IpAddr { kind: IpAddKind::V4, address: String::from("127.0.0.1") };
-        let loopback = IpAddr { kind: IpAddKind::V6, address: String::from("::1") };
+        let home = IpAddr {
+            kind: IpAddKind::V4,
+            address: String::from("127.0.0.1"),
+        };
+        let loopback = IpAddr {
+            kind: IpAddKind::V6,
+            address: String::from("::1"),
+        };
 
         println!("{:#?}, {:#?}", home, loopback)
     }
@@ -68,5 +74,80 @@ fn main() {
         // let x: i8 = 5;
         // let y: Option<i8> = Some(5);
         // let sum = x + y;
+    }
+
+    // matchフロー制御演算子
+    {
+        enum Coin {
+            Penny,
+            Nickel,
+            Dime,
+            Quarter,
+        }
+
+        fn value_in_cents(coin: Coin) -> u32 {
+            match coin {
+                Coin::Penny => {
+                    println!("Lucky penny!");
+                    1
+                },
+                Coin::Nickel => 5,
+                Coin::Dime => 10,
+                Coin::Quarter => 25,
+            }
+        }
+
+        println!("{}", value_in_cents(Coin::Dime));
+        println!("{}", value_in_cents(Coin::Penny));
+        println!("{}", value_in_cents(Coin::Nickel));
+        println!("{}", value_in_cents(Coin::Quarter));
+    }
+
+    // 値に束縛されるパターン
+    {
+        #[derive(Debug)]
+        enum UsState {
+            Alabama,
+            Alaska,
+        }
+
+        enum Coin {
+            Penny,
+            Nickel,
+            Dime,
+            Quarter(UsState),
+        }
+
+        fn value_in_cents(coin: Coin) -> u32 {
+            match coin {
+                Coin::Penny => 1,
+                Coin::Nickel => 5,
+                Coin::Dime => 10,
+                Coin::Quarter(state) => {
+                    println!("State quarter from {:?}", state);
+                    25
+                }
+            }
+        }
+        println!("{}", value_in_cents(Coin::Dime));
+        println!("{}", value_in_cents(Coin::Penny));
+        println!("{}", value_in_cents(Coin::Nickel));
+        println!("{}", value_in_cents(Coin::Quarter(UsState::Alabama)));
+        println!("{}", value_in_cents(Coin::Quarter(UsState::Alaska)));
+    }
+
+    // Option<T>とのマッチ
+    {
+        fn plus_one(x: Option<i32>) -> Option<i32> {
+            match x {
+                None => None,
+                Some(i) => Some(i + 1),
+            }
+        }
+
+        let five = Some(5);
+        let six = plus_one(five);
+        let none = plus_one(None);
+        println!("{:?} {:?}", six, none);
     }
 }
