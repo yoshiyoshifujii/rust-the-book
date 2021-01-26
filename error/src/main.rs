f447e491e9013b0788838e2216350b23a8b5305f
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2021-01-26 20:18:42 +0900

    update

--- error/src/main.rs
+++ error/src/main.rs
@@ -32,8 +32,35 @@ fn main() {
 
     // エラー時にパニックするショートカット: unwrapとexpect
     // let f = File::open("hello.txt").unwrap();
-    let f = File::open("hello.txt").expect("Failed to open hello.txt");
-    println!("{:?}", f);
+    // let f = File::open("hello.txt").expect("Failed to open hello.txt");
+    // println!("{:?}", f);
 
+    // エラーを委譲する
+    // use std::io;
+    // use std::io::Read;
+    // fn read_username_from_file() -> Result<String, io::Error> {
+    //     let f = File::open("hello.txt");
+    //     let mut f = match f {
+    //         Ok(file) => file,
+    //         Err(e) => return Err(e),
+    //     };
+    //     let mut s = String::new();
+    //     match f.read_to_string(&mut s) {
+    //         Ok(_) => Ok(s),
+    //         Err(e) => Err(e),
+    //     }
+    // }
+    // read_username_from_file().unwrap();
+
+    // エラー委譲のショートカット: ?演算子
+    use std::io;
+    use std::io::Read;
+    fn read_username_from_file() -> Result<String, io::Error> {
+        let mut f = File::open("hello.txt")?;
+        let mut s = String::new();
+        f.read_to_string(&mut s)?;
+        Ok(s)
+    }
+    read_username_from_file().unwrap();
 }
 
