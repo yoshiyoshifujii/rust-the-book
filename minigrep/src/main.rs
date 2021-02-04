6438f34c4ca5496cbac4b0cfdce220be493a5e38
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2021-02-04 21:14:31 +0900

    update minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,20 +1,20 @@
-use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+extern crate minigrep;
+
+use std::{env, process};
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-
-    let mut f = File::open(filename).expect("file not found");
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("something went wrong reading the file");
-
-    println!("With text :\n{}", contents);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
+
