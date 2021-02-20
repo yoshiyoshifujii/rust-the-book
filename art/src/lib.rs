f61c2bdbf8cc0e31346bb13a82b790a18f7e0ef0
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2021-02-20 22:45:28 +0900

    add art

---
+++ art/src/lib.rs
@@ -0,0 +1,28 @@
+//! Art
+//!
+//! A library for modeling artistic concepts.
+
+pub mod kinds {
+    /// The primary colors according to the RYB color model.
+    pub enum PrimaryColor {
+        Red,
+        Yellow,
+        Blue,
+    }
+
+    /// RYBカラーモデルによる副色
+    pub enum SecondaryColor {
+        Orange,
+        Green,
+        Purple,
+    }
+}
+
+pub mod utils {
+    use crate::kinds::{PrimaryColor, SecondaryColor};
+
+    /// 2つの主色を同じ割合で混合し、副色にする
+    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+        unimplemented!()
+    }
+}
